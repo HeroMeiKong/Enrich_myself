b86d3497270a6fcb3fcd7851a9ddfe76facb5f2a
xiaopang <xiaopangfeng4@hotmail.com>
2018-08-15 16:38:36 +0800

    add loading

--- Netease-cloud-music-mobile/src/js/admin/event-hub.js
+++ Netease-cloud-music-mobile/src/js/admin/event-hub.js
@@ -1,8 +1,8 @@
 window.eventHub = {
   events: {
-
+    //hash
   },
-  emit(eventName,data){
+  emit(eventName,data){//发布
     for(let key in this.events){
       if(key === eventName){
         let fnList = this.events[key]
@@ -12,7 +12,7 @@ window.eventHub = {
       }
     }
   },
-  on(eventName,fn){
+  on(eventName,fn){//订阅
     if(this.events[eventName] === undefined){
       this.enents[eventName] = []
     }
